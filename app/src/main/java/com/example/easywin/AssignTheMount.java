9293fe210d9d384150366260917e63b10941214e
Михаил Зыков <mizykov.edu.hse.ru>
2019-09-15 02:58:53 +0300

    Assign lobby

--- app/src/main/java/com/example/easywin/AssignTheMount.java
+++ app/src/main/java/com/example/easywin/AssignTheMount.java
@@ -1,10 +1,15 @@
 package com.example.easywin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -33,13 +38,43 @@ public class AssignTheMount extends AppCompatActivity {
             View child = inflater.inflate(R.layout.user_row, listViewNames, false);
 
             ((TextView)child.findViewById(R.id.name)).setText(names[i]);
+            ((Button) child.findViewById(R.id.assignMoney)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DialogFragment am = new AssignMoney();
+                    am.show(getSupportFragmentManager(), "missiles");
+                }
+            });
             child.findViewById(R.id.transOverNamesList).setVisibility(View.VISIBLE);
             listViewNames.addView(child);
         }
     }
 
     public  void onClick(View view){
-        findViewById(R.id.transOverlay).setVisibility(View.VISIBLE);
+        //findViewById(R.id.transOverlay).setVisibility(View.VISIBLE);
+    }
+
+    public static class AssignMoney extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+                    .setPositiveButton("signin", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            // sign in the user ...
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //LoginDialogFragment.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
     }
 
 }
+

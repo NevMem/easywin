03a1fefda7775066547a88ecbda7d272ead3b54a
yudin2109 <yudin2109@gmail.com>
2019-09-15 02:03:03 +0300

    Some cosmetic changes

--- app/src/main/java/com/example/easywin/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/easywin/fragments/InfoFragment.kt
@@ -8,29 +8,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.easywin.JoinActivity
 import com.example.easywin.R
+import kotlinx.android.synthetic.main.activity_user_info.*
+import kotlinx.android.synthetic.main.info_item.*
 import kotlinx.android.synthetic.main.main_page.view.*
 
-class MainPageFragment : Fragment() {
+class InfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_page, container, false)
+        return inflater.inflate(R.layout.info_item, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.join_lobby_button.setOnClickListener {
-            var intent = Intent(context, JoinActivity::class.java)
-            startActivity(intent)
-        }
-
-        view.topPanel.setOnClickListener {
-            var intent = Intent(context, UserInfoActivity::class.java)
-            startActivity(intent)
-        }
     }
 
 }

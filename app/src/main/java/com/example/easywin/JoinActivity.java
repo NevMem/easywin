dcf5c4c8738a61c02b9b2f5d702aca413068bf38
bulumutka <mishasdk@gmail.com>
2019-09-14 17:08:01 +0300

    Join activity top design created

--- app/src/main/java/com/example/easywin/JoinActivity.java
+++ app/src/main/java/com/example/easywin/JoinActivity.java
@@ -3,6 +3,10 @@ package com.example.easywin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import kotlin.NotImplementedError;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+    }
+
+    public void onJoinButtonClick(View view) {
+        throw new NotImplementedError();
+    }
+
+    public void onQrButtonClick(View view) {
+        throw new NotImplementedError();
+    }
+
+
+
+    private String getTextFromEditText(View view) {
+        EditText editText = (EditText) findViewById(R.id.meeting_id);
+        String meetingName = editText.getText().toString();
+        return meetingName;
     }
 }
